506b08b1a3b4e8842ca5b307fe109841cdab7ab2
PabloCastro2004 <pablojcperalta99778@gmail.com>
2024-09-17 17:44:27 -0600

    Update postController.js

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -2,15 +2,8 @@ const data = require('../data/data');
 const { getPostsQuery } = require('../models/postModel');
 
 exports.getPost = (req, res) => {
-  const { id } = req.params;
-  publicacion = data.posts.find((item) => item.id == id);
   res.json({
-    id: publicacion.id,
-    title: publicacion.title,
-    content: publicacion.content,
-    course: publicacion.course,
-    teacher: publicacion.teacher,
-    userId: publicacion.userId,
+    mensaje: "controlador getpost"
   });
 };
 
